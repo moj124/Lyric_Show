79be9940c740d2cd2c76160c51373e6c223d26c2
michael jones <michael.jowen.jones@gmail.com>
2021-09-01 16:11:24 +0100

    added scraping

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { greet } from "./utils/greet";
+import {getLyrics} from './utils/getLyrics';
 
 function App(): JSX.Element {
+  getLyrics()
   return <h1>{greet("World")}</h1>;
 }
 
---
+++ src/utils/getLyrics.ts
@@ -0,0 +1,55 @@
+// Loading the dependencies. We don't need pretty
+// because we shall not log html to the terminal
+const axios = require("axios");
+const cheerio = require("cheerio");
+const fs = require("fs");
+
+// URL of the page we want to scrape
+const url = "https://www.azlyrics.com/lyrics/maverickcitymusic/champion.html";
+
+// Async function which scrapes the data
+async function getLyrics() {
+  try {
+    // Fetch HTML of the page we want to scrape
+    const { data } = await axios.get(url);
+    // Load HTML we fetched in the previous line
+    const $ = cheerio.load(data);
+    // Select all the list items in plainlist class
+    const listItems = $("div .text-center");
+    // Stores data for all songs
+    const songs: {name:string,text:string}[] = [];
+    // Use .each method to loop through the li we selected
+    listItems.each((idx: number, el:string) => {
+      // Object holding data for each song/jurisdiction
+      const song = { name: "", text: "" };
+      // Select the text content of a and span elements
+      // Store the textcontent in the above object
+      song.name = $(el).children("b").text().replace(/['"]+/g, "");
+      song.text = $(el).children("div:nth-of-type(5)").text();
+      // Populate songs array with song data
+      if (song.text.length !== 0) {
+        songs.push(song);
+      }
+    });
+    // Logs songs array to the console
+    console.dir(songs);
+    // Write songs array in songs.json file
+    fs.writeFile(
+      "./src/" + songs[0].name.toLowerCase() + ".json",
+      JSON.stringify(songs, null, 2),
+      (err:string) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log("Successfully written data to file");
+      }
+    );
+  } catch (err) {
+    console.error(err);
+  }
+}
+// Invoke the above function
+// getLyrics();
+export{}
+
