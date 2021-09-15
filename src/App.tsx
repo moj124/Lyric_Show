e8bf15a211331b2f266917485b2060d7f9095e90
michael jones <michael.jowen.jones@gmail.com>
2021-09-15 18:50:16 +0100

    getLyrics fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import { greet } from "./utils/greet";
-import {getLyrics} from './utils/getLyrics';
+import champion1 from './champion1.json';
+import how_great_is_our_god from './how_great_is_our_god.json';
+import way_2_sexy from './way_2_sexy.json'
 
 function App(): JSX.Element {
-  getLyrics()
-  return <h1>{greet("World")}</h1>;
+  // const {name, text} = champion1[0];
+  // const {name, text} = how_great_is_our_god[0];
+  const  {name, text}  = way_2_sexy[0];
+
+  const lines = text.map((element,index) => element.length > 1 ? <p key={index}>{element}</p>: <br></br>)
+
+  return (
+    <main>
+      <h1>{name}</h1>
+      {lines}
+    </main>);
 }
 
 export default App;
--- src/utils/getLyrics.js
+++ src/utils/getLyrics.js
@@ -1,43 +1,45 @@
 // Loading the dependencies. We don't need pretty
 // because we shall not log html to the terminal
-import axios from "axios";
-import cheerio from "cheerio";
-import fs from "fs";
+const axios = require("axios");
+const cheerio = require("cheerio");
+const fs = require("fs");
+
 
 // URL of the page we want to scrape
-const url = "https://www.azlyrics.com/lyrics/maverickcitymusic/champion.html";
-
+// const url = "https://www.azlyrics.com/lyrics/maverickcitymusic/champion.html";
+// const url = "https://www.azlyrics.com/lyrics/christomlin/howgreatisourgod.html";
+const url = 'https://www.azlyrics.com/lyrics/drake/way2sexy.html';
 // Async function which scrapes the data
- async function getLyrics() {
+ async function getLyrics(url) {
   try {
     // Fetch HTML of the page we want to scrape
     const { data } = await axios.get(url);
     // Load HTML we fetched in the previous line
     const $ = cheerio.load(data);
     // Select all the list items in plainlist class
-    const listItems = $("div .text-center");
+    const listItems= $("div .text-center");
     // Stores data for all songs
-    const songs: {name:string,text:string}[] = [];
+    const songs = [];
     // Use .each method to loop through the li we selected
-    listItems.each((idx: number, el:string) => {
+    listItems.each((idx, el) => {
       // Object holding data for each song/jurisdiction
       const song = { name: "", text: "" };
       // Select the text content of a and span elements
       // Store the textcontent in the above object
       song.name = $(el).children("b").text().replace(/['"]+/g, "");
-      song.text = $(el).children("div:nth-of-type(5)").text();
+      song.text = $(el).children("div:nth-of-type(5)").text().split('\n');
       // Populate songs array with song data
-      if (song.text.length !== 0) {
+      if (song.text.length > 1) {
         songs.push(song);
       }
     });
     // Logs songs array to the console
-    console.dir(songs);
+    console.log(songs);
     // Write songs array in songs.json file
     fs.writeFile(
       "./src/" + songs[0].name.toLowerCase() + ".json",
       JSON.stringify(songs, null, 2),
-      (err:unknown) => {
+      (err) => {
         if (err) {
           console.error(err);
           return;
@@ -50,6 +52,5 @@ const url = "https://www.azlyrics.com/lyrics/maverickcitymusic/champion.html";
   }
 }
 // Invoke the above function
-// getLyrics();
-export {getLyrics}
+getLyrics(url);
 
